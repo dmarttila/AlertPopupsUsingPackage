7a47f4efdae8701b47f59a0b5da9a1293679e0ab
Doug Marttila <dmarttila@gmail.com>
2021-12-27 09:49:41 -0500

    Added package and some sample alerts

--- AlertPopupsUsingPackage/ContentView.swift
+++ AlertPopupsUsingPackage/ContentView.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import AlertPopUp_iOS13
 
 struct ContentView: View {
+    @State private var alertParams: AlertParams?
+    
+    func destroySomething () {
+        print ("OMG Destroying!")
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(alertParams?.message ?? "alerParams is currently nil, which means the alert does not show")
+            Button("Alert with one button and action") {
+                alertParams = AlertParams(title: "One Button", message: "Only One Button", showTwoButtons: false, primaryButtonLabel: "Single button func") { print("Single button func") }
+            }
+            Button("Alert with one button and no func") {
+                alertParams = AlertParams(title: "One button", message: "Clicking OK will only close the alert")
+            }
+            
+            Button("Alert with two buttons desctructive") {
+                alertParams = AlertParams(title: "Destructive Title", message: "Desctrucive description", showTwoButtons: true, primaryButtonLabel: "Destroy!", destructive: true, primaryButtonAction: destroySomething)
+            }
+            
+            Button("Alert with two buttons not destructive") {
+                alertParams = AlertParams(title: "Non-destructive", message: "Two buttons", showTwoButtons: true, primaryButtonLabel: "Non-destructive ") { print("No destruction here") }
+            }
+        }
+        .modifier(AlertModifier(alertParams: $alertParams))
+        .padding()
     }
 }
 
