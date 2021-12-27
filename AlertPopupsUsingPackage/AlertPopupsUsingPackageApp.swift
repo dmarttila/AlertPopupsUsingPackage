11147c95cb565565018dab9331561ebc5794855f
Doug Marttila <dmarttila@gmail.com>
2021-12-27 09:16:51 -0500

    Initial Commit

---
+++ AlertPopupsUsingPackage/AlertPopupsUsingPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertPopupsUsingPackageApp.swift
+//  AlertPopupsUsingPackage
+//
+//  Created by Douglas Marttila on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct AlertPopupsUsingPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
